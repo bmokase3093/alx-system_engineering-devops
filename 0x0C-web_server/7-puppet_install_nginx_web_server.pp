a441568ff87bf54d099184cd4b2cffb3d21cb9fb
bmokase3093 <bmokase3093@gmail.com>
2022-09-30 06:38:42 -0700

    configure nginx using puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,17 @@
-# Using Puppet| Install Nginx server, setup and configuration
-
+# Puppet manifest to install nginx
 package { 'nginx':
-  ensure => 'installed'
+  ensure => installed,
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-file_line { 'redirection-301':
+file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
 service { 'nginx':
